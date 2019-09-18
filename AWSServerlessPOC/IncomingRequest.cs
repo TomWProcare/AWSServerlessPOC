ccbced9aa942dd962f358adbfcd7abea357709dd
Tom Wingert <thw@procaresoftware.com>
2019-09-17 17:00:42 -0700

    More code clean up.

--- AWSServerlessPOC/IncomingRequest.cs
+++ AWSServerlessPOC/IncomingRequest.cs
@@ -1,19 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
-using Amazon;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.SQSEvents;
-using Amazon.SimpleEmail;
-using Amazon.SimpleEmail.Model;
-using Amazon.SQS;
 using Amazon.SQS.Model;
-using Amazon.Util;
 using Newtonsoft.Json;
-using Message = Amazon.SimpleEmail.Model.Message;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
@@ -32,18 +25,19 @@ namespace AWSServerlessPOC
         /// A Lambda function to respond to HTTP Get methods from API Gateway
         /// </summary>
         /// <param name="request"></param>
+        /// <param name="context"></param>
         /// <returns>The list of blogs</returns>
         public async Task<APIGatewayProxyResponse> Post(APIGatewayProxyRequest request, ILambdaContext context)
         {
             context.Logger.LogLine("ASync post request \n");
-            var SQS_QUEUE_URL = Environment.GetEnvironmentVariable("MY_SQS_QUEUE_URL");
-            var SQS_ACCOUNT_ID = Environment.GetEnvironmentVariable("MY_SQS_ACCOUNT_ID");
-            var SQS_QUEUE_NAME = Environment.GetEnvironmentVariable("MY_SQS_QUEUE_NAME");
-            var queueURL = SQS_QUEUE_URL + SQS_ACCOUNT_ID + "/" + SQS_QUEUE_NAME;
-            context.Logger.LogLine("URL is: " + queueURL);
+            var sqsQueueUrl = Environment.GetEnvironmentVariable("MY_SQS_QUEUE_URL");
+            var sqsAccountId = Environment.GetEnvironmentVariable("MY_SQS_ACCOUNT_ID");
+            var sqsQueueName = Environment.GetEnvironmentVariable("MY_SQS_QUEUE_NAME");
+            var queueUrl = $"{sqsQueueUrl}{sqsAccountId}/{sqsQueueName}";
+            context.Logger.LogLine("URL is: " + queueUrl);
             using (var sqsClient = new Amazon.SQS.AmazonSQSClient())
             {
-                var sqsRequest = new SendMessageRequest {QueueUrl = queueURL, MessageBody = request.Body};
+                var sqsRequest = new SendMessageRequest {QueueUrl = queueUrl, MessageBody = request.Body};
                 var sqsMessageResponse = await sqsClient.SendMessageAsync(sqsRequest);
             }
 
